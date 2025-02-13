5a7816cfa43c2e09edee2e940cb55a2dcc92d1e5
Your Name <you@example.com>
2025-02-12 19:13:03 -0500

    test

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gcc -c -Os -fno-ident -fno-asynchronous-unwind-tables -fno-stack-protector -fomit-frame-pointer -o shell.o shell.c
+as sys.S -o sys.o
+ld shell.o sys.o -o init -T custom.ld --strip-all -z noexecstack
